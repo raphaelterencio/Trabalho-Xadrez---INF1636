796df7435df0ad987c3286cd4136503f6e45773d
Matheus Figueiredo <108003682+mfigueireddo@users.noreply.github.com>
2025-05-23 20:30:05 -0300

    getPossibleMoves implementada

--- inf1636-xadrez/src/model/Piece.java
+++ inf1636-xadrez/src/model/Piece.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.List;
+import java.util.ArrayList;
 
 abstract class Piece 
 {
@@ -37,6 +38,25 @@ abstract class Piece
 	{
 		this.row = row;
 		this.column = column;
+	}
+	
+	protected List<int[]> getPossibleMoves() 
+	{
+	    List<int[]> possibleMoves = new ArrayList<>();
+
+	    for (int target_row = 0; target_row < 8; target_row++) {
+	        for (int target_column = 0; target_column < 8; target_column++) 
+	        {
+	            // Ignora a posição atual da peça
+	            if (target_row == this.row && target_column == this.column) continue;
+
+	            if (canMove(target_row, target_column)) {
+	                possibleMoves.add(new int[]{target_row, target_column});
+	            }
+	        }
+	    }
+
+	    return possibleMoves;
 	}
 	
 	protected abstract boolean canMove(int target_row, int target_column);
