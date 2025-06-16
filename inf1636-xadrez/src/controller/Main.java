332e8208177e2ef1d281e97b8f626765690beb13
Matheus Figueiredo <108003682+mfigueireddo@users.noreply.github.com>
2025-06-16 00:54:25 -0300

    Peças se movendo

--- inf1636-xadrez/src/controller/Main.java
+++ inf1636-xadrez/src/controller/Main.java
@@ -5,11 +5,22 @@ import view.ViewAPI;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main
 {
 	static ModelAPI model_api;
 	static ViewAPI view_api;
+	
+	static boolean isPieceSelected = false;
+	static int selected_row = -1;
+	static int selected_column = -1;
+	
+	static int origin_row = -1;
+	static int origin_column = -1;
+	
+	static List<int[]> highlighted_path = new ArrayList<>();
 	
 	public static void main(String[] args)
 	{	
@@ -22,6 +33,8 @@ public class Main
 		userClickHandler();
 	}
 	
+	// Callbacks
+	
 	private static void userClickHandler()
 	{
         view_api.addMouseListener(new MouseAdapter() {
@@ -31,14 +44,53 @@ public class Main
                 int x = e.getX();
                 int y = e.getY();
                 
-                int row = y / 64;
-                int column = x / 64;
+                selected_row = y / 64;
+                selected_column = x / 64;
                 
-                if (model_api.isThereAPiece(row, column))
-                	view_api.highlightPath(row, column);
-                else
-                	view_api.clearHighlightedPath();
+                if ( !isPieceSelected )
+                {
+                	if (model_api.isThereAPiece(selected_row, selected_column))
+                	{
+                		origin_row = selected_row;
+                		origin_column = selected_column;
+                		view_api.highlightPath(selected_row, selected_column);
+                		highlighted_path = model_api.getPossibleMoves(selected_row, selected_column);
+                		isPieceSelected = !isPieceSelected;
+                	}
+                }
+                else 
+                {
+                	if ( isHighlighted(selected_row, selected_column) )
+                	{
+                		model_api.movePiece(origin_row, origin_column, selected_row, selected_column);
+                		selected_row = -1; selected_column = -1;
+                		origin_row = -1; origin_column = -1;
+                		view_api.clearHighlightedPath();
+                	}
+                	else
+                	{
+                		selected_row = -1; selected_column = -1;
+                		origin_row = -1; origin_column = -1;
+                		view_api.clearHighlightedPath();
+                	}
+                	isPieceSelected = !isPieceSelected;
+                }
+                
+                
             }
         });
+    }
+	
+	// Auxiliares
+	
+    private static boolean isHighlighted(int row, int column)
+    {
+        for (int[] pos : highlighted_path) {
+            if (pos[0] == row && pos[1] == column) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
